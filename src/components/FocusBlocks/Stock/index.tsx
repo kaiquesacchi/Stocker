1025b791c40984c431899163a1d74aa663710f6d
Kaique Sacchi <sacchikaique@gmail.com>
2021-01-26 23:45:49 -0300

    Added StockFocusBlock component.

---
+++ src/components/FocusBlocks/Stock/index.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import BaseFocusBlock, { iProps as iPropsBase } from "../Base";
+import styled from "styled-components/native";
+
+interface iSCFontColor {
+  fontColor: string;
+}
+
+const SCBody = styled.View`
+  padding: 20px 0;
+`;
+
+const SCMainInfo = styled.View<iSCFontColor>`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+  border-bottom-width: 1px;
+  border-color: ${(props) => props.fontColor + "50"};
+`;
+const SCMainLeft = styled.View``;
+const SCMainRight = styled.View`
+  align-items: flex-end;
+`;
+
+const SCSymbolText = styled.Text<iSCFontColor>`
+  color: ${(props) => props.fontColor};
+  font-size: 20px;
+`;
+const SCNameText = styled.Text<iSCFontColor>`
+  color: ${(props) => props.fontColor};
+  opacity: 0.8;
+`;
+const SCCurrentText = styled.Text<iSCFontColor>`
+  color: ${(props) => props.fontColor};
+  font-size: 20px;
+`;
+
+interface iSCChangeBadge {
+  isPositive: boolean;
+}
+const SCChangeBadge = styled.View<iSCChangeBadge>`
+  background-color: ${(props) => (props.isPositive ? "#49aa49" : "#d35b3d")};
+  padding: 5px 8px;
+  border-radius: 20px;
+  border: 1px solid #ddd;
+  margin-top: 5px;
+`;
+
+const SCChangeText = styled.Text`
+  color: white;
+`;
+
+const SCAmountText = styled.Text<iSCFontColor>`
+  color: ${(props) => props.fontColor};
+`;
+
+export interface iStockData {
+  symbol: string;
+  companyName: string;
+  currentPrice: string;
+  todayChange: string;
+  changeIsPositive: boolean;
+  averagePrice: string;
+  amount: string;
+  totalYield: string;
+}
+
+interface iProps extends iPropsBase {
+  stockData: iStockData;
+  fontColor: string;
+}
+
+export default function StockFocusBlock({ stockData, fontColor, ...rest }: iProps) {
+  return (
+    <BaseFocusBlock {...rest}>
+      <SCBody>
+        <SCMainInfo fontColor={fontColor}>
+          <SCMainLeft>
+            <SCSymbolText fontColor={fontColor}>{stockData.symbol}</SCSymbolText>
+            <SCNameText fontColor={fontColor}>{stockData.companyName}</SCNameText>
+          </SCMainLeft>
+          <SCMainRight>
+            <SCCurrentText fontColor={fontColor}>{stockData.currentPrice}</SCCurrentText>
+            <SCChangeBadge isPositive={stockData.changeIsPositive}>
+              <SCChangeText>{stockData.todayChange}</SCChangeText>
+            </SCChangeBadge>
+          </SCMainRight>
+        </SCMainInfo>
+        <SCAmountText
+          fontColor={fontColor}>{`Valor total: ${stockData.totalYield} (${stockData.amount})`}</SCAmountText>
+      </SCBody>
+    </BaseFocusBlock>
+  );
+}
--- src/pages/MyWallet/index.tsx
+++ src/pages/MyWallet/index.tsx
@@ -1,13 +1,41 @@
 import React from "react";
 import { View } from "react-native";
 import { Navigation } from "../../components/BottomBars";
+import StockFocusBlock, { iStockData } from "../../components/FocusBlocks/Stock";
 
 import { AppBarPage } from "../../components/Pages";
 
 export default function MyWallet() {
+  const stockDataList: iStockData[] = [
+    {
+      symbol: "ABCD4",
+      companyName: "Empresa A. B.",
+      currentPrice: "R$36,51",
+      todayChange: "-0,12 (-1,3%)",
+      changeIsPositive: false,
+      averagePrice: "R$32,00",
+      amount: "200 cotas",
+      totalYield: "R$ 800,00",
+    },
+    {
+      symbol: "EFGH11",
+      companyName: "Empresa E. F.",
+      currentPrice: "R$12,11",
+      todayChange: "3,12 (+9,4%)",
+      changeIsPositive: true,
+      averagePrice: "R$20,13",
+      amount: "600 cotas",
+      totalYield: "R$ -1803,01",
+    },
+  ];
+
   return (
     <View style={{ flex: 1 }}>
-      <AppBarPage title="Minha Carteira"></AppBarPage>
+      <AppBarPage title="Minha Carteira" hasNavigationBar>
+        {stockDataList.map((stockData, index) => (
+          <StockFocusBlock key={index} stockData={stockData} fontColor="#ffffff" />
+        ))}
+      </AppBarPage>
       <Navigation />
     </View>
   );
