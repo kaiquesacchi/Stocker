3977eedc3c2f25605c00e6ca0d19eee31a5d8657
Kaique Sacchi <sacchikaique@gmail.com>
2021-02-08 17:22:26 -0300

    Added dev-module Reactotron.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import Router from "./src/pages";
+
 import { ActiveTabContextProvider } from "./src/context/ActiveTab";
+
+if (__DEV__) {
+  import("./dev/ReactotronConfig");
+}
 
 export default function App() {
   return (
